4413205ecc216d029a7c3222eca20e2a65fc5053
Rohit Chauhan <162285388+Rohit0410@users.noreply.github.com>
2024-08-27 14:49:21 +0530

    Add files via upload
    Resume filling and User creation combined

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def preprocessing(document):
 def get_gemini_response(input_text,prompt):
     model=genai.GenerativeModel('gemini-1.5-flash')
     response=model.generate_content([input_text,prompt],generation_config = genai.GenerationConfig(
-        temperature=0.5
+        temperature=0.1
     ))
     return response.text
 
@@ -58,35 +58,57 @@ Ensure the data is as it appears in the resume, and if certain details are not a
 
 Data=[
 - Designation:
-- Years of Experience:
+- Years of Experience(calculate it if not mentioned directly, only numeric number):
 - Current Organization:
 
-- Skills (List all relevant and valid skills mentioned):
+- Skills (List all relevant and valid skills mentioned. "," seperated):
 
 - Education History (from oldest to latest):
-    [- Name of the Institution:
-    - Degree:
-    - Field of Study:
-    - Grade (if available):
-    - Duration (Start Year - End Year):
-    - Description (if available):
-    ]
+    {NameInstitution: ,
+    Degree: ,
+    FieldStudy: ,
+    Grade (if available): ,
+    Start Year: , 
+    End Year: ,
+    Description (if available): ,}//n
 
 - Professional Experience (from oldest to latest):
-    [- Company Name:
-    - Role/Title:
-    - Location:
-    - Employment Type (e.g., Full-time, Part-time, Internship):
-    - Duration (Start Date - End Date):
-    - Skills Used:
-    - Description of Roles and Responsibilities:
-    ]
+    {Company Name: ,
+    Role/Title: ,
+    Location: ,
+    Employment Type (e.g., Full-time, Part-time, Internship): ,
+    Start Date: , 
+    End Date: ,
+    Skills Used for this job: ,
+    Description of Roles and Responsibilities: ,
+    }//n
 
 - First Name:
 - Last Name:
+- Current Location: 
 ]
 
 """
+
+key_mapping_edu = {
+    "Degree": "degree",
+    "Description (if available)": "description",
+    "Start Year": "startDate",
+    "End Year": "endDate",
+    "FieldStudy": "fieldOfStudy",
+    "Grade (if available)": "grade",
+    "NameInstitution": "institute"
+}
+
+key_mapping_prof = {"Company Name":"organization" ,
+    "Role/Title":"designation" ,
+    "Location":"location" ,
+    "Employment Type (e.g., Full-time, Part-time, Internship)":"employmentType" ,
+    "Start Date":"startDate" ,
+    "End Date":"endDate" ,
+    "Skills Used":"skillUsed" ,
+    "Description of Roles and Responsibilities":"description",
+    }
 
 @app.route('/score_resumes', methods=['POST'])
 def scoring():
@@ -104,6 +126,7 @@ def scoring():
         print('jd_file_path',jd_file_path)
         resumes.save(jd_file_path)
         
+        # for i in resume_paths:
         data1,input_text = input_pdf_setup(jd_file_path)
         if input_text is None:
             return jsonify({'error': 'Error processing the JD file.'}), 500
@@ -122,17 +145,78 @@ def scoring():
         emails = email_pattern.findall(data1)
         emails=str(emails)
         social_media_links = social_media_pattern.findall(data1)
-        social_media_links=str(social_media_links)
+        social_media_links=list(social_media_links)
         valid_phone_numbers = [num for num in phone_numbers if 10 <= len(re.sub(r'\D', '', num)) <= 12]
+
         print(valid_phone_numbers)
 
-        response = get_gemini_response(input_text,  Prompt)
-         
+        Country_code=''
+
+        if len(valid_phone_numbers[0])==10:
+            Country_code = ''
+            valid_phone_numbers=valid_phone_numbers[0]
+        elif len(valid_phone_numbers[0])>=12:
+            Country_code = valid_phone_numbers[0][1:3]
+            valid_phone_numbers=valid_phone_numbers[0][-10:]
+
+        
+
+        response = get_gemini_response(input_text,Prompt)
+        
         t=response.split("\n-")
-        final = {'Designation':t[1].replace("Designation:","").replace('\n',''),'Years of Experience':t[2].replace("Years of Experience:","").replace('\n',''),"Current Organization":t[3].replace("Current Organization:","").replace('\n',''),
-                 "Skills (List all relevant and valid skills mentioned)":t[4].replace("Skills (List all relevant and valid skills mentioned):","").replace('\n',''),"Education History (from oldest to latest)":t[5].replace("Education History (from oldest to latest):","").replace('\n',''),
-                 "Professional Experience (from oldest to latest)":t[6].replace("Professional Experience (from oldest to latest):",""),'First Name':t[7].replace("First Name:","").replace('\n','').replace('[','').replace(']',''),'Last Name':t[8].replace("Last Name:","").replace('\n','').replace('[','').replace(']',''),
-                 'Phone Number':str(valid_phone_numbers).replace('[','').replace(']',''),"Email":emails.replace('[','').replace(']',''),"social media":social_media_links.replace('[','').replace(']','')}
+        print(type(t[5]))
+
+        Designation = t[1].replace("Designation:","").replace('\n','')
+        total_exp=t[2].replace("Years of Experience:","").replace('\n','')
+        organization=t[3].replace("Current Organization:","").replace('\n','')
+        skills=t[4].replace("Skills (List all relevant and valid skills mentioned):","").replace('\n','').replace("Skills:","")
+        name=t[7].replace("First Name:","").replace('\n','').replace('[','').replace(']','')
+        surname=t[8].replace("Last Name:","").replace('\n','').replace('[','').replace(']','')
+        phone=str(valid_phone_numbers).replace('[','').replace(']','')
+        country=Country_code.replace('[','').replace(']','')
+        email=emails.replace('[','').replace(']','')
+        social=social_media_links #.replace('[','').replace(']','')
+        location=t[9].replace("Current Location:","").replace('\n','').replace('[','').replace(']','')
+
+        educationHistory=t[5].replace("Education History (from oldest to latest):","").replace('\n','').replace('Education History:',"").replace("{","").replace("}","")
+        edu=[element for element in educationHistory.split("//n") if len(element) > 0]
+        final_edu=[]
+        for i in edu:
+            pairs = i.strip().split(',')
+            result_dict={}
+            for pair in pairs:
+                if ':' in pair:  # Ensure there is a key-value pair to split
+                    key, value = pair.split(":", 1)  # Split by the first occurrence of ":"
+                    result_dict[key.strip()] = value.strip()
+                    new_dict = {key_mapping_edu[key]: value for key, value in result_dict.items() if key in key_mapping_edu}
+            final_edu.append(new_dict)
+        
+ 
+        professionalExperience=t[6].replace("Professional Experience (from oldest to latest):","").replace("{","").replace("}","").replace("Professional Experience:","")
+        prof=[element for element in professionalExperience.split("//n") if len(element) > 0]
+        final_prof=[]
+        for i in prof:
+            pairs = i.strip().split(',')
+            result_dict={}
+            for pair in pairs:
+                if ':' in pair:  # Ensure there is a key-value pair to split
+                    key, value = pair.split(":", 1)  # Split by the first occurrence of ":"
+                    result_dict[key.strip()] = value.strip()
+                    new_dict = {key_mapping_prof[key]: value for key, value in result_dict.items() if key in key_mapping_prof}
+            final_prof.append(new_dict)
+
+        final = {'designation':Designation,
+                 'totalExperience':int(total_exp),
+                 "organization":organization,
+                 "SkillsInfo":skills,
+                 'firstName':name,
+                 'lastName':surname,
+                 'phoneNo':int(phone),
+                 "email":email,
+                 "socialInfo":social,
+                 "countryCode":int(country),
+                 'location':location,
+                 "educationHistory":final_edu, "professionalHistory":final_prof,"yearOfExperience":int(total_exp)}
         print(len(t))
         print(t)
 
@@ -143,4 +227,4 @@ def scoring():
         return jsonify({'error': 'An internal server error occurred.'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(debug=True, port=5005)
